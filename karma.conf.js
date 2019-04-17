38ac43e3011c2bcb2bdebbab41472de2ef763854
eburtin <elodie.burtin@cytomine.coop>
2019-04-17 16:52:45 +0200

    Update dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/babel-polyfill/dist/polyfill.js',
+      'node_modules/@babel/polyfill/dist/polyfill.js',
       'tests/test-*.js'
     ],
 
