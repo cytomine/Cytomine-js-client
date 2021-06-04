cf4a7e3e92b9d238716ad6b13cc938342de29dd0
lrollus <loic.rollus@cytomine.com>
2021-06-04 13:31:23 +0200

    publication with token (wip)

--- scripts/ciPublish.sh
+++ scripts/ciPublish.sh
@@ -9,13 +9,14 @@ file='./ci/version'
 VERSION_NUMBER=$(<"$file")
 
 echo "Publish npm for $VERSION_NUMBER"
+echo "$NPM_TOKEN"
 
 if [[ $VERSION_NUMBER =~ [0-9]+.[0-9]+.[0-9]$ ]]; then
   echo "Official release"
 
   docker build --rm -f scripts/docker/Dockerfile-publish.build -t cytomine/cytomine-js-client-publish \
           --build-arg VERSION_NUMBER=$VERSION_NUMBER  \
-          --build-arg NPM_KEY=$NPM_KEY  .
+          --build-arg NPM_TOKEN=$NPM_TOKEN  .
 
   containerId=$(docker create cytomine/cytomine-js-client-publish)
 
