5d103c678eb45922afcf577af4f7298bbbcb322b
lrollus <loic.rollus@cytomine.com>
2022-03-24 10:06:11 +0100

    build

--- scripts/ciPublish.sh
+++ scripts/ciPublish.sh
@@ -12,14 +12,19 @@ echo "Publish npm for $VERSION_NUMBER"
 echo "$NPM_TOKEN"
 
 
-if [[ $VERSION_NUMBER =~ [0-9]+.[0-9]+.[0-9]$ ]]; then
-  echo "Official release"
+if [[ $VERSION_NUMBER =~ v[0-9]+.[0-9]+.[0-9]+-beta.[0-9]+-0-[0-9a-g]{8,9}$ ]]; then
+  echo "BETA"
+  TAG=beta
+elif [[ $VERSION_NUMBER =~ [0-9]+.[0-9]+.[0-9]$ ]]; then
+  echo "OFFICIAL"
   TAG=latest
 else
-  echo "beta release"
-  TAG=beta
+  echo "WARNING: invalid tag for an official release, do not publish; if you want to publish a beta: write something like this: git tag -a v1.2.3-beta.3"
+  TAG=none
 fi
 
+
+if [[ $TAG -ne "none" ]]; then
 docker build --rm -f scripts/docker/Dockerfile-publish.build -t cytomine/cytomine-js-client-publish \
         --build-arg VERSION_NUMBER=$VERSION_NUMBER  \
         --build-arg TAG=$TAG  \
@@ -28,7 +33,7 @@ docker build --rm -f scripts/docker/Dockerfile-publish.build -t cytomine/cytomin
 containerId=$(docker create cytomine/cytomine-js-client-publish)
 
 docker rm $containerId
-
+fi
 #else
 #  echo "Snapshot, not publishing it...to be implemented"
 #fi
