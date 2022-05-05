639e08f8236af4cffde45018c648cf22325fa266
Paul Barthe <p.barthe441@hotmail.com>
2022-05-05 11:58:50 +0200

    feat: fetch followers of specific user in specific image

--- src/collections/user-collection.js
+++ src/collections/user-collection.js
@@ -1,6 +1,6 @@
 import Collection from './collection.js';
 import User from '../models/user.js';
-
+import Cytomine from '../cytomine.js';
 export default class UserCollection extends Collection {
 
   /** @inheritdoc */
@@ -11,5 +11,20 @@ export default class UserCollection extends Collection {
   /** @inheritdoc */
   static get allowedFilters() {
     return [null, 'project', 'ontology'];
+  }
+
+  /**
+   * @static Fetch followers
+   *
+   * @param {String} userId  The user id of user who is followed
+   * @param {String} imageId The image id
+   *
+   * @returns {UserCollection} Collection of users following user (userId) on image (imageId)
+   */
+  static async fetchFollowers(userId, imageId) {
+    let {data} = await Cytomine.instance.api.get(`imageinstance/${imageId}/followers/${userId}.json`);
+    let userCollection = new UserCollection();
+    data.collection.forEach(item => userCollection.push(new User(item)));
+    return userCollection.collection;
   }
 }
