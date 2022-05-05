be254912ef4984fb83d76a1b65be88a2c1d61cdc
Ulysse Rubens <urubens@uliege.be>
2022-05-05 17:14:01 +0200

    style: method position (changed during merge)

--- src/models/annotation.js
+++ src/models/annotation.js
@@ -142,18 +142,6 @@ export default class Annotation extends Model {
     return reviewedAnnotation;
   }
 
-  async repeat(slice, number) {
-    if(this.isNew()) {
-      throw new Error('Cannot repeat an annotation with no ID.');
-    }
-
-    let {data} = await Cytomine.instance.api.post(`userannotation/${this.id}/repeat.json`, {
-      slice,
-      repeat: number
-    });
-    return data;
-  }
-
   /**
    * Cancel the review of the current annotation (thus deleting associated reviewed annotations)
    */
@@ -221,6 +209,18 @@ export default class Annotation extends Model {
     let {data} = await Cytomine.instance.api.post('annotationcorrection.json', params);
     Cytomine.instance.lastCommand = data.command;
     return new this(data.annotation || data.reviewedannotation);
+  }
+
+  async repeat(slice, number) {
+    if(this.isNew()) {
+      throw new Error('Cannot repeat an annotation with no ID.');
+    }
+
+    let {data} = await Cytomine.instance.api.post(`userannotation/${this.id}/repeat.json`, {
+      slice,
+      repeat: number
+    });
+    return data;
   }
 
   /** @inheritdoc */
