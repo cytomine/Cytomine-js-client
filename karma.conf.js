870ef21bed60a4376b747628640e7621dbc0c9cd
Ulysse Rubens <urubens@uliege.be>
2022-05-20 16:41:48 +0200

    fix: use regenerator-runtime instead of deprecated babel/polyfill https://stackoverflow.com/a/54490329

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/@babel/polyfill/dist/polyfill.js',
+      'src/index.js',
       'tests/test-*.js'
     ],
 
