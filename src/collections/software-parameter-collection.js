af09595e5016add3ca58ee496c32fefe2e1d0c3c
Ba Thien Le <bathien.le.pro@gmail.com>
2023-05-22 14:39:20 +0200

    Revert "Fix software parameter collection API endpoint"
    This reverts commit 31db9fb6331d5cde39e3fcdab0519c1c20e48958.

--- src/collections/software-parameter-collection.js
+++ src/collections/software-parameter-collection.js
@@ -17,10 +17,10 @@ export default class SoftwareParameterCollection extends Collection {
   /** @inheritdoc */
   get uri() {
     if(this._filter.key === 'software' && this._filter.value) {
-      return `software/${this._filter.value}/parameter.json`;
+      return `software/${this._filter.value}/software_parameter.json`;
     }
     else {
-      return 'softwareparameter.json';
+      return 'software_parameter.json';
     }
   }
 }
