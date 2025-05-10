2b02fc575c058885a4115a67c1b0ae9582415e9a
Ba Thien LE <bathien.le.pro@gmail.com>
2025-05-10 16:00:11 +0200

    test: add code coverage

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,7 +1,26 @@
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.js',
+  ],
+  coverageReporters: [
+    'html-spa',
+    'text-summary',
+  ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  reporters: [
+    [
+      'jest-html-reporter',
+      {
+        outputPath: './reports/test-report.html',
+        pageTitle: 'Test Report',
+        includeFailureMsg: true,
+        includeConsoleLog: true,
+      },
+    ],
+  ],
   testEnvironment: 'jsdom',
   testRegex: '(/tests/.*\\.test\\.js)$',
   transform: {
