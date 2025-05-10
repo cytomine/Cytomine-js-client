b8702d5f8bc3f53af17ab20d82c27f318020b6bc
Ba Thien LE <bathien.le.pro@gmail.com>
2025-05-10 20:30:19 +0200

    test: remove deprecated tests

--- tests/image-server.test.js
+++ tests/image-server.test.js
@@ -2,43 +2,11 @@ import * as utils from './utils.js';
 import { ImageServer, ImageServerCollection } from '@/index.js';
 
 describe('ImageServer', function () {
-  let totalNb = 0;
-
-  let imageServer;
-
   beforeAll(async function () {
     await utils.connect(true);
   });
 
-  describe('Fetch collection', function () {
-    it('Fetch (instance method)', async function () {
-      let collection = await new ImageServerCollection().fetchAll();
-      expect(collection).toBeInstanceOf(ImageServerCollection);
-      totalNb = collection.length;
-      imageServer = collection.get(0);
-    });
-
-    it('Fetch (static method)', async function () {
-      let collection = await ImageServerCollection.fetchAll();
-      expect(collection).toBeInstanceOf(ImageServerCollection);
-      expect(collection).toHaveLength(totalNb);
-    });
-
-    it('Fetch with several requests', async function () {
-      let collection = await ImageServerCollection.fetchAll({ nbPerPage: Math.ceil(totalNb / 3) });
-      expect(collection).toBeInstanceOf(ImageServerCollection);
-      expect(collection).toHaveLength(totalNb);
-    });
-  });
-
   describe('Working with the collection', function () {
-    it('Iterate through', async function () {
-      let collection = await ImageServerCollection.fetchAll();
-      for (let imageServer of collection) {
-        expect(imageServer).toBeInstanceOf(ImageServer);
-      }
-    });
-
     it('Add item to the collection', function () {
       let collection = new ImageServerCollection();
       expect(collection).toHaveLength(0);
@@ -51,23 +19,4 @@ describe('ImageServer', function () {
       expect(collection.push.bind(collection, {})).toThrow();
     });
   });
-
-  describe('Fetch', function () {
-    it('Fetch with static method', async function () {
-      let fetchedImageServer = await ImageServer.fetch(imageServer.id);
-      expect(fetchedImageServer).toBeInstanceOf(ImageServer);
-      expect(fetchedImageServer).toEqual(imageServer);
-    });
-
-    it('Fetch with instance method', async function () {
-      let fetchedImageServer = await new ImageServer({ id: imageServer.id }).fetch();
-      expect(fetchedImageServer).toBeInstanceOf(ImageServer);
-      expect(fetchedImageServer).toEqual(imageServer);
-    });
-
-    it('Fetch with wrong ID', function () {
-      expect(ImageServer.fetch(0)).rejects.toThrow();
-    });
-  });
-
 });
